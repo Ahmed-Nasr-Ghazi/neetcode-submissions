f50d7862eb102b5e30bd40af89438ab771b40d50
Ahmed Nasr <73040374+Ahmed-Nasr-Ghazi@users.noreply.github.com>
2026-09-24 16:29:08 +0300

    Add: majority-element - submission-9

---
+++ Data Structures & Algorithms/majority-element/submission-9.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> result = new HashMap<>();
+        int res = 0; int max = 0;
+        for (int num : nums) {
+            result.put(num, result.getOrDefault(num, 0) + 1);
+            if (result.get(num) > max) {
+                max = result.get(num);
+                res = num;
+            }
+        }
+        return res;
+    }
+}
